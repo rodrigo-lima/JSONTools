229488e489203e39fbbe0aab222a86541cc63596
Rodrigo Lima <rodrigo.lima@oracle.com>
2016-12-18 15:56:51 -0600

    Add JSONTools Frameworks for Carthage

--- JSONTools/JSONTools.h
+++ JSONTools/JSONTools.h
@@ -7,6 +7,13 @@
 
 @import Foundation;
 
-#import "JSONPointer.h"
-#import "JSONPatch.h"
+//! Project version number for JSONTools.
+FOUNDATION_EXPORT double JSONToolsVersionNumber;
+
+//! Project version string for JSONTools.
+FOUNDATION_EXPORT const unsigned char JSONToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONTools/PublicHeader.h>
+#import <JSONTools/JSONPointer.h>
+#import <JSONTools/JSONPatch.h>
 // #import "JSONSchema.h" -- Not ready yet
